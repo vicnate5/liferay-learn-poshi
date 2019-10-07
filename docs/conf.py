94f542a0c892939ee5108e2c2db482a65482fb70
nikki-pru <nikki.prudencio@liferay.com>
2019-10-07 09:04:34 -0700

    LRQA-51977 Set Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ release = u''
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -73,6 +73,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+import sphinx_rtd_theme
+
 html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
